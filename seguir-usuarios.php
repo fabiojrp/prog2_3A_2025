c07628e766080fac4a2d73107d7c78174b452120
fabiojrp <fabio.pinheiro@ifc.edu.br>
2025-10-13 11:46:30 -0300

    ok

---
+++ seguir-usuarios.php
@@ -0,0 +1,76 @@
+<?php
+    include "incs/valida-sessao.php";
+?>
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Página Inicial</title>
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+</head>
+<body>
+
+  <!-- Navbar -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+      <a class="navbar-brand" href="#">Meu Projeto</a>      
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item"><a class="nav-link active" href="#">Início</a></li>
+          <li class="nav-item"><a class="nav-link" href="#">Sobre</a></li>
+          <li class="nav-item"><a class="nav-link" href="#">Contato</a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <!-- Conteúdo Principal -->
+  <main class="container my-5 w-75">
+    <h3 class="text-center">Escolha seus amigos</h3>
+    <form action="" class="w-75 mx-auto text-start row">
+        <div class="mb-3 col-8">
+            <label class="form-label">Nome do usuário</label>
+            <input type="text" class="form-control" name="nome" placeholder="Nome">
+        </div>
+        <div class="mb-3 col-4">
+            <button type="submit" class="btn btn-dark btn-lg my-4">Buscar</button>    
+        </div>
+    
+      <div class="row w-50 my-3">
+      <?php
+        require_once "src/UsuarioDAO.php";
+        
+        
+        if (isset($_GET['nome'])){            
+            $usuarios = UsuarioDAO::buscarUsuario($_SESSION['idusuario'], $_GET['nome']);
+            //$usuarios = UsuarioDAO::buscarUsuarioParaSeguir($_SESSION['idusuario'], $_GET['nome']);
+        }
+
+        foreach ($usuarios as $usuario) {
+            ?>
+        
+        <div class="mb-2 col-8">
+          <?=$usuario['nome']?>
+        </div>
+        <div class="mb-2 col-4">
+          <a href="seguir.php?idseguido=<?=$usuario['idusuario']?>" class="btn btn-success mx-3">
+            Adicionar
+          </a>
+        </div>
+        
+        <?php
+        }
+        
+        ?>
+        </div>
+    </form>
+  </main>
+
+  <!-- Footer -->
+  <footer class="bg-dark text-white text-center py-3 mt-auto">
+    <p class="mb-0">&copy; 2025 - Minha Página. Todos os direitos reservados.</p>
+  </footer>
+</body>
+</html>
---
+++ src/UsuarioDAO.php
@@ -0,0 +1,78 @@
+<?php
+require_once "ConexaoBD.php";
+
+class UsuarioDAO{
+
+    public static function cadastrarUsuario($dados){
+        $conexao = ConexaoBD::conectar();
+        
+        $sql = "insert into usuarios (email, senha, nome) values (?,?,?)";
+        $stmt = $conexao->prepare($sql);        
+        $stmt->bindParam(1, $dados['email']);
+        $senhaCriptografada = md5($dados['senha']);
+        $stmt->bindParam(2, $senhaCriptografada);
+        $stmt->bindParam(3, $dados['nome']);
+
+        $stmt->execute();
+    }
+
+    public static function validarUsuario($dados){
+        
+        $senhaCriptografada = md5($dados['senha']);
+        $sql = "select * from usuarios where email=? AND senha=?";
+
+        $conexao = ConexaoBD::conectar();
+        $stmt = $conexao->prepare($sql);
+        $stmt->bindParam(1, $dados['email']);
+        $stmt->bindParam(2, $senhaCriptografada);
+        $stmt->execute();
+        $usuario = $stmt->fetch(PDO::FETCH_ASSOC);  
+        if ($stmt->rowCount() > 0){
+            return $usuario['idusuario'];
+        }else{
+            return false;
+        }
+    }
+
+    public static function listarUsuarios($idusuario){
+        $sql = "select * from usuarios where idusuario!=?";
+
+        $conexao = ConexaoBD::conectar();
+        $stmt = $conexao->prepare($sql);
+        $stmt->bindParam(1, $idusuario);
+        $stmt->execute();
+                
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public static function buscarUsuario($idusuario, $nome){
+        $sql = "select * from usuarios where idusuario!=? and nome like ?";
+
+        $conexao = ConexaoBD::conectar();
+        $stmt = $conexao->prepare($sql);
+        $stmt->bindParam(1, $idusuario);
+        $nome = "%".$nome."%";
+        $stmt->bindParam(2, $nome);
+        $stmt->execute();
+                
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public static function buscarUsuarioParaSeguir($idusuario, $nome){
+        $sql = "SELECT u.* FROM usuarios u 
+        WHERE u.idusuario != ? AND u.nome like ?
+        AND u.idusuario NOT IN 
+        (SELECT s.idseguido FROM seguidos s WHERE s.idusuario = ?);";
+
+        $conexao = ConexaoBD::conectar();
+        $stmt = $conexao->prepare($sql);
+        $stmt->bindParam(1, $idusuario);
+        $nome = "%" . $nome . "%";
+        $stmt->bindParam(2, $nome);
+        $stmt->bindParam(3, $idusuario);
+        $stmt->execute();
+
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
+?>
